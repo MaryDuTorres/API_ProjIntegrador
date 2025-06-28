91bc38ef5e5dfb90bfa39b60f8549a9c53efa75e
Maria Eduarda Torres <maria.pereira96@fatec.sp.gov.br>
2025-06-28 02:40:10 +0000

    a

--- routes/contaRoutes.js
+++ routes/contaRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const contaController = require('../controllers/contaController');
 
 router.post('/registrar', contaController.registrar); 
-router.post('/login', contaController.login); 
-router.put('/editarConta:id', contaController.editarConta);
+router.post('/login', contaController.login);
+router.put('/editarConta/:id', contaController.editarConta);
 
 module.exports = router;
